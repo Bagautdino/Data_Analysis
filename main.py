766160a599c7ce08207a76681c6daaffe6bfafb8
Bagautdino <51779102+Bagautdino@users.noreply.github.com>
2021-10-13 18:37:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
 import json
-# Открываем файл
-f = open("pokemon_full.json")
-json_str = f.read()
-data = json.loads(json_str)
-print("1. Общая длина файла равна: {",len(json_str),"}") # 1. Общая длина файла равна: { 413889 }
 
-#Задаём знаки препинания которые хотим убрать
+file = open("pokemon_full.json")
+pokemon_str = file.read()
+json_object = json.loads(pokemon_str)
+print(f"1. Общая длина файла равна: {len(pokemon_str)}")
+
 punctuations = '''‘’“”«»?‐…‒–—―⟨ ⟩''’!()-[]{};:'"\,<>./#$%^&*_~␠␢␣@©'''
 
-#Будет формироваться новая строка
 new_string = ''
-for i in json_str:
+for i in pokemon_str:
     if i not in punctuations:
-        new_string+=i
-#Убираем переходы на новую строку
-new_string = new_string.replace("\n","")
-print("2. Общая длина без пробелов и знаков препинания {", len(new_string),"}") # 2. Общая длина без пробелов
-                                                                                # и знаков препинания { 176636 }
+        new_string += i
+new_string = new_string.replace("\n", "")
+print(f"2. Общая длина без пробелов и знаков препинания: {len(new_string)}")
 
-
-#Находим покемона с самым длинным описанием
-most_d = 0
-pokemon = ''
-for item in data:
-    description = (item['description'])
-    if len(description) > most_d:
-        most_d = len(description)
-        pokemon = item['name']
-print(f"3. Покемон {pokemon} с самым длинным описанием {most_d} символов") # 3. Покемон Mew с самым длинным описанием
-                                                                           # 171 символов
-
-#Найдем самое длинное название умений
-most_a = 0
-most_ability = ''
-for abilities in data:
-    ability = (abilities['abilities'])
-    if (len(str.split(''.join(ability)))) > most_a:
-        most_a = len(str.split(''.join(ability)))
-        most_ability = ''.join(ability)
-print(f"4. Самое большое количество умений {most_a}: {most_ability}") # 4. Самое большое количество умений 6:
-                                                                      # Keen EyeSand RushSteadfastKeen EyeVital
-                                                                      # SpiritNo Guard
+max_desc = 0
+max_amount = 0
+poke_name = ""
+res_ability = ""
+for pokemon in json_object:
+    desc = pokemon["description"]
+    for ability_amount in pokemon["abilities"]:
+        if len(ability_amount.split()) > max_amount:
+            max_amount = len(ability_amount.split())
+            res_ability = ability_amount
+    if len(desc) > max_desc:
+        max_desc = len(desc)
+        poke_name = pokemon["name"]
+print(f"3. Покемон {poke_name} с самым длинным описанием {max_desc} символов")
+print(f"4. Самое большое количество умений: {res_ability}")
+file.close()
