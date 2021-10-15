91d97bfb16753936ca40b1f26250d57b841712f6
Bagautdino <51779102+Bagautdino@users.noreply.github.com>
2021-10-15 13:13:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ print(f"1. Общая длина файла равна: {len(pokemon_str)}")
 
 punctuations = '''‘’“”«»?‐…‒–—―⟨ ⟩''’!()-[]{};:'"\,<>./#$%^&*_~␠␢␣@©'''
 
-new_string = ''
+str_without_punc = ""
 for i in pokemon_str:
     if i not in punctuations:
-        new_string += i
-new_string = new_string.replace("\n", "")
+        str_without_punc += i
+new_string = str_without_punc.replace("\n", "")
 print(f"2. Общая длина без пробелов и знаков препинания: {len(new_string)}")
 
 max_desc = 0
@@ -20,13 +20,13 @@ poke_name = ""
 res_ability = ""
 for pokemon in json_object:
     desc = pokemon["description"]
-    for ability_amount in pokemon["abilities"]:
-        if len(ability_amount.split()) > max_amount:
-            max_amount = len(ability_amount.split())
-            res_ability = ability_amount
+    for ability_str in pokemon["abilities"]:
+        if len(ability_str.split()) > max_amount:
+            max_amount = len(ability_str.split())
+            res_ability = ability_str
     if len(desc) > max_desc:
         max_desc = len(desc)
         poke_name = pokemon["name"]
 print(f"3. Покемон {poke_name} с самым длинным описанием {max_desc} символов")
-print(f"4. Самое большое количество умений: {res_ability}")
+print(f"4. Самое большое количество слов: {res_ability}")
 file.close()
